90a99a1c91e4e8288242c91882cd34ce232203c2
AnneThor <anne.thor@gmail.com>
2021-05-22 17:15:54 -0700

    added help page, final UML

--- src/pages/help/help.js
+++ src/pages/help/help.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import HelpDocs from './Help.md'
 import './help.scss';
 
 class Help extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = { help: null }
+  }
+
+  componentDidMount() {
+    fetch(HelpDocs)
+      .then(res => res.text())
+      .then(text => {
+        this.setState({ help: text })
+      })
+  }
 
   render() {
 
     return (
       <div id="help">
-        <h1>How to Use Our App</h1>
-
+        <ReactMarkdown children={this.state.help} />
       </div>
     )
   }
